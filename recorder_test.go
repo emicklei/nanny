9b12c59360f690abf6ebbdfeeee24c9bcc0cc049
Ernest Micklei <ernest@ag5.com>
2023-11-27 22:41:58 +0100

    add 2 retention strategies for remove old events

--- recorder_test.go
+++ recorder_test.go
@@ -1,6 +1,7 @@
 package nanny
 
 import (
+	"fmt"
 	"log/slog"
 	"testing"
 )
@@ -19,4 +20,27 @@ func TestGroupMarker(t *testing.T) {
 	g.Debug("question", "answer", 42)
 
 	rec.Log()
+}
+
+func TestMaxEventGroups(t *testing.T) {
+	rec := NewRecorder()
+	rec.retentionStrategy = MaxEventGroupsStrategy{MaxEventGroups: 2}
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			rec.Record(slog.LevelDebug, fmt.Sprintf("grp%d", i), fmt.Sprintf("msg%d", j), nil)
+		}
+	}
+	if got, want := len(rec.groupSet), 2; got != want {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
+	grps := rec.buildGroups() // includes the empty group
+	if got, want := len(grps), 3; got != want {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
+	if got, want := grps[1].name, "grp3"; got != want {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
+	if got, want := grps[2].name, "grp4"; got != want {
+		t.Errorf("got [%v]:%T want [%v]:%T", got, got, want, want)
+	}
 }
