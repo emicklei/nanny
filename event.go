1f3e6236508e4d9e8083337da3969e454c26bc90
Ernest Micklei <ernest.micklei@gmail.com>
2023-11-30 21:04:29 +0100

    change setup default

--- event.go
+++ event.go
@@ -14,10 +14,15 @@ type Event struct {
 	Attrs   map[string]any `json:"a" `
 }
 
-// SetupDefault wraps the default log handler with a handler that records events (window=100).
+// SetupDefault wraps the default log handler with a handler that records events.
+// Logging all events on error detection is true.
+// Maxium number of event groups in memory is 100.
 // Installs the browser (page=100) on the default serve mux with path /nanny
 func SetupDefault() {
-	rec := NewRecorder(WithMaxEvents(100), WithGroupMarkers("func"))
+	rec := NewRecorder(
+		WithLogEventGroupOnError(true),
+		WithMaxEventGroups(100),
+		WithGroupMarkers("func"))
 	reclog := slog.New(NewLogHandler(rec, slog.Default().Handler(), LevelTrace))
 	slog.SetDefault(reclog)
 	http.Handle("/nanny", NewBrowser(rec, WithPageSize(100)))
